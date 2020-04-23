3e39da47faaa28d0cf0ff3d5f1e519ad1b6b6330
root <dokken92@gmail.com>
2020-04-23 10:16:12 +0100

    Revert typos in build script

--- lib/src/build.sh
+++ lib/src/build.sh
@@ -2,4 +2,4 @@
 g++ -Wall -fPIC -shared `python3 -m pybind11 --includes` -I/usr/include/eigen3 openGJK.cpp -o opengjk`python3-config --extension-suffix`
 g++ -Wall -fPIC -shared `python3 -m pybind11 --includes` -I/usr/include/eigen3 openGJK.c -o opengjkc`python3-config --extension-suffix`
 
-# g++ -Wall -fPIC -shared -I/usr/include/python8 -I/home/fenics/.local/include -I../include -I/usr/include/eigen3 openGJK.c -o opengjkc`python3-config --extension-suffix`
+#  g++ -Wall -fPIC -shared -I/usr/include/python3.6m -I/home/chris/.local/include -I../include -I/usr/include/eigen3 openGJK.c -o opengjkc`python3-config --extension-suffix` 
