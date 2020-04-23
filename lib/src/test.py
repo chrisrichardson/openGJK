8693d8559c410eca0158986e9f63db125d77faea
root <dokken92@gmail.com>
2020-04-23 07:51:32 +0000

    Added more tests

--- lib/src/test.py
+++ lib/src/test.py
@@ -54,11 +54,25 @@ def test_line_point_distance(delta):
     actual_distance = distance_point_to_line_3D(
         line[0], line[1], point)
     print(distance, actual_distance)
-    assert(np.isclose(distance, actual_distance))
+    assert(np.isclose(distance, actual_distance, atol=1e-15))
+
+
+@pytest.mark.parametrize("delta", [0.1, 1e-12, 0])
+def test_line_line_distance(delta):
+    line = np.array([[-0.5, -0.7, -0.3], [1, 2, 3]], dtype=np.float64)
+    point_on_line = line[0] + 0.38*(line[1]-line[0])
+    normal = np.cross(line[0], line[1])
+    point = point_on_line + delta * normal
+    line_2 = np.array([point, [2, 5, 6]], dtype=np.float64)
+    distance = opengjk.gjk(line, line_2)
+    actual_distance = distance_point_to_line_3D(
+        line[0], line[1], line_2[0])
+    print(distance, actual_distance)
+    assert(np.isclose(distance, actual_distance, atol=1e-15))
 
 
 @pytest.mark.parametrize("delta", [0.1**(3*i) for i in range(6)])
-def test_tri_collision2d(delta):
+def test_tri_distance(delta):
     tri_1 = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]], dtype=np.float64)
     tri_2 = np.array([[1, delta, 0], [3, 1.2, 0], [
         1, 1, 0]], dtype=np.float64)
@@ -68,7 +82,22 @@ def test_tri_collision2d(delta):
     actual_distance = distance_point_to_line_3D(P1, P2, point)
     distance = opengjk.gjk(tri_1, tri_2)
     print("Computed distance ", distance, actual_distance)
-    assert(np.isclose(distance, actual_distance))
+    assert(np.isclose(distance, actual_distance, atol=1e-15))
+
+
+@pytest.mark.parametrize("delta", [0.1*0.1**(3*i) for i in range(6)])
+def test_quad_distance2d(delta):
+    quad_1 = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0],
+                       [1, 1, 0]], dtype=np.float64)
+    quad_2 = np.array([[0, 1+delta, 0], [2, 2, 0],
+                       [2, 4, 0], [4, 4, 0]], dtype=np.float64)
+    P1 = quad_1[2]
+    P2 = quad_1[3]
+    point = quad_2[0]
+    actual_distance = distance_point_to_line_3D(P1, P2, point)
+    distance = opengjk.gjk(quad_1, quad_2)
+    print("Computed distance ", distance, actual_distance)
+    assert(np.isclose(distance, actual_distance, atol=1e-15))
 
 
 @pytest.mark.parametrize("delta", [1*0.5**(3*i) for i in range(7)])
@@ -81,10 +110,10 @@ def test_tetra_distance_3d(delta):
                                                  tetra_1[2], tetra_2[3])
     distance = opengjk.gjk(tetra_1, tetra_2)
     print("Computed distance ", distance, actual_distance)
-    assert(np.isclose(distance, actual_distance))
+    assert(np.isclose(distance, actual_distance, atol=1e-15))
 
 
-@pytest.mark.parametrize("delta", [1*0.1**(3*i) for i in range(6)])
+@pytest.mark.parametrize("delta", [np.sqrt(2)*0.1**(3*i) for i in range(6)])
 def test_tetra_collision_3d(delta):
     tetra_1 = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0],
                         [0, 0, 1]], dtype=np.float64)
@@ -94,4 +123,4 @@ def test_tetra_collision_3d(delta):
                                                  tetra_1[2], tetra_2[3])
     distance = opengjk.gjk(tetra_1, tetra_2)
     print("Computed distance ", distance, actual_distance)
-    assert(np.isclose(distance, actual_distance))
+    assert(np.isclose(distance, actual_distance, atol=1e-15))
