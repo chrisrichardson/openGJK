a406329b0851e03917a19b29d7b0f4cc8e33512f
Chris Richardson <chris@bpi.cam.ac.uk>
2020-05-14 13:51:20 +0100

    Work in progress

--- lib/src/openGJK.cpp
+++ lib/src/openGJK.cpp
@@ -36,63 +36,64 @@ namespace
 {
 /// @brief Finds point of minimum norm of 1-simplex. Robust, but slower,
 /// version of algorithm presented in paper.
-std::pair<std::array<int, 3>, Eigen::Vector3d> S1D(const Simplex& s, int r0,
-                                                   int r1)
+std::pair<std::array<int, 4>, Eigen::Vector3d>
+S1D(const Eigen::Matrix<double, 4, 3, Eigen::RowMajor>& s, int r0, int r1)
 {
-  const Eigen::RowVector3d t = s.vrtx.row(r0) - s.vrtx.row(r1);
+  const Eigen::RowVector3d t = s.row(r0) - s.row(r1);
 
   const double tnorm2 = t.squaredNorm();
   if (tnorm2 == 0.0)
     throw std::runtime_error("t=0 in S1D");
 
   // Project origin onto the 1D simplex - line
-  const double pt = s.vrtx.row(r0).dot(t) / tnorm2;
+  const double pt = s.row(r0).dot(t) / tnorm2;
 
   if (pt >= 0.0 and pt <= 1.0)
   {
     // The origin is between A and B
-    Eigen::Vector3d v = s.vrtx.row(r0) - pt * t;
-    return {{r0, r1, -1}, v};
+    Eigen::Vector3d v = s.row(r0) - pt * t;
+    return {{r0, r1, -1, -1}, v};
   }
 
   // The origin is beyond A, change point
   if (pt > 1.0)
-    return {{r1, -1, -1}, s.vrtx.row(r1)};
+    return {{r1, -1, -1, -1}, s.row(r1)};
   else
-    return {{r0, -1, -1}, s.vrtx.row(r0)};
+    return {{r0, -1, -1, -1}, s.row(r0)};
 }
 
 /// @brief Finds point of minimum norm of 2-simplex. Robust, but slower,
 /// version of algorithm presented in paper.
-std::pair<std::array<int, 3>, Eigen::Vector3d> S2D(Simplex& s, int r0, int r1,
-                                                   int r2)
+std::pair<std::array<int, 4>, Eigen::Vector3d>
+S2D(const Eigen::Matrix<double, 4, 3, Eigen::RowMajor>& s, int r0, int r1,
+    int r2)
 {
   Eigen::Matrix3d M;
-  M.row(0) = s.vrtx.row(r0);
-  M.row(1) = s.vrtx.row(r1);
-  M.row(2) = s.vrtx.row(r2);
+  M.row(0) = s.row(r0);
+  M.row(1) = s.row(r1);
+  M.row(2) = s.row(r2);
   const Eigen::Vector3d ac = M.row(0) - M.row(2);
   const Eigen::Vector3d bc = M.row(0) - M.row(1);
 
   // Find best axis for projection
-  Eigen::Vector3d n = ac.cross(bc);
-  if (n.squaredNorm() == 0.0)
+  Eigen::Vector3d nvrtx = ac.cross(bc);
+  if (nvrtx.squaredNorm() == 0.0)
     throw std::runtime_error("Zero normal in S2D");
-  const Eigen::Vector3d nu_fabs = n.cwiseAbs();
+  const Eigen::Vector3d nu_fabs = nvrtx.cwiseAbs();
   int indexI;
   nu_fabs.maxCoeff(&indexI);
-  const double nu_max = n[indexI];
+  const double nu_max = nvrtx[indexI];
 
   // Barycentre of triangle
   Eigen::Vector3d p = M.colwise().sum() / 3.0;
-  // Renormalise n in plane of ABC
-  n *= n.dot(p) / n.squaredNorm();
+  // Renormalise nvrtx in plane of ABC
+  nvrtx *= nvrtx.dot(p) / nvrtx.squaredNorm();
 
   const int indexJ0 = (indexI + 1) % 3;
   const int indexJ1 = (indexI + 2) % 3;
   const Eigen::Vector3d W1 = M.col(indexJ0).reverse();
   const Eigen::Vector3d W2 = M.col(indexJ1).reverse();
-  const Eigen::Vector3d W3 = (W1 * n[indexJ1] - W2 * n[indexJ0]);
+  const Eigen::Vector3d W3 = (W1 * nvrtx[indexJ1] - W2 * nvrtx[indexJ0]);
   Eigen::Vector3d B = W1.cross(W2);
   B[0] += (W3[2] - W3[1]);
   B[1] += (W3[0] - W3[2]);
@@ -106,35 +107,31 @@ std::pair<std::array<int, 3>, Eigen::Vector3d> S2D(Simplex& s, int r0, int r1,
   if ((FacetsTest[0] + FacetsTest[1] + FacetsTest[2]) == 3)
   {
     // The origin projection lays onto the triangle
-    s.nvrtx = 3;
-    return {{r0, r1, r2}, n};
+    return {{r0, r1, r2, -1}, nvrtx};
   }
 
-  std::array<int, 3> arrmin = {-1, -1, -1};
-  Eigen::Vector3d vmin;
+  std::array<int, 4> arrmin = {-1, -1, -1, -1};
+  Eigen::Vector3d vmin = {0, 0, 0};
   if (FacetsTest[1] == 1 and FacetsTest[2] == 1)
   {
     // FacetsTest[0] == 0
     // The origin projection P faces the segment BC
-    std::tie(arrmin, vmin) = S1D(s, 0, 1);
+    std::tie(arrmin, vmin) = S1D(s, r0, r1);
   }
 
   double qmin = std::numeric_limits<double>::max();
-  for (int i = 0; i < 2; ++i)
+  if (FacetsTest[1] == 0)
   {
-    if (FacetsTest[i + 1] == 0)
+    std::tie(arrmin, vmin) = S1D(s, r0, r2);
+    qmin = vmin.squaredNorm();
+  }
+  if (FacetsTest[2] == 0)
+  {
+    auto [arr, v] = S1D(s, r1, r2);
+    if (v.squaredNorm() < qmin)
     {
-      Eigen::Vector3d v;
-      std::array<int, 3> arr;
-      // Edge (0, 2) or (1, 2)
-      std::tie(arr, v) = S1D(s, i, 2);
-      const double q = v.squaredNorm();
-      if (q < qmin)
-      {
-        qmin = q;
-        arrmin = arr;
-        vmin = v;
-      }
+      arrmin = arr;
+      vmin = v;
     }
   }
 
@@ -143,22 +140,21 @@ std::pair<std::array<int, 3>, Eigen::Vector3d> S2D(Simplex& s, int r0, int r1,
 
 /// @brief Finds point of minimum norm of 3-simplex. Robust, but slower,
 /// version of algorithm presented in paper.
-Eigen::Vector3d S3D(Simplex& s)
+std::pair<std::array<int, 4>, Eigen::Vector3d>
+S3D(const Eigen::Matrix<double, 4, 3, Eigen::RowMajor>& s)
 {
-  assert(s.nvrtx == 4);
-
   Eigen::Vector4d B;
-  const Eigen::Vector3d W1 = s.vrtx.row(0).cross(s.vrtx.row(1));
-  const Eigen::Vector3d W2 = s.vrtx.row(2).cross(s.vrtx.row(3));
-  B[0] = s.vrtx.row(2) * W1;
-  B[1] = -s.vrtx.row(3) * W1;
-  B[2] = s.vrtx.row(0) * W2;
-  B[3] = -s.vrtx.row(1) * W2;
+  const Eigen::Vector3d W1 = s.row(0).cross(s.row(1));
+  const Eigen::Vector3d W2 = s.row(2).cross(s.row(3));
+  B[0] = s.row(2) * W1;
+  B[1] = -s.row(3) * W1;
+  B[2] = s.row(0) * W2;
+  B[3] = -s.row(1) * W2;
 
   double detM = B.sum();
 
   // Test if sign of ABCD is equal to the signs of the auxiliary simplices
-  const double eps = 1e-15;
+  const double eps = 0;
   int FacetsTest[4] = {1, 1, 1, 1};
   if (std::abs(detM) < eps)
   {
@@ -187,69 +183,41 @@ Eigen::Vector3d S3D(Simplex& s)
       FacetsTest[i] = (std::signbit(detM) == std::signbit(B[i]));
   }
 
-  // Compare signed volumes and compute barycentric coordinates
   if (FacetsTest[0] + FacetsTest[1] + FacetsTest[2] + FacetsTest[3] == 4)
   {
     // All signs are equal, therefore the origin is inside the simplex
-    s.nvrtx = 4;
-    return Eigen::Vector3d::Zero();
+    return {{0, 1, 2, 3}, Eigen::Vector3d::Zero()};
   }
 
   if (FacetsTest[1] + FacetsTest[2] + FacetsTest[3] == 3)
   {
     // The origin projection P faces the facet BCD
-    s.nvrtx = 3;
-    Eigen::Vector3d v;
-    std::array<int, 3> arr;
-    std::tie(arr, v) = S2D(s, 0, 1, 2);
-    int n = 0;
-    for (n = 0; n < 3; ++n)
-    {
-      if (arr[n] == -1)
-        break;
-      s.vrtx.row(n) = s.vrtx.row(arr[n]);
-    }
-    s.nvrtx = n;
+    return S2D(s, 0, 1, 2);
   }
 
-  // Either 1, 2 or 3 of ACD, ABD or ABC are closest.
-  Simplex sTmp[2];
-  Eigen::Vector3d vBest;
+  // Test ACD, ABD and/or ABC.
+  std::array<int, 4> arrmin = {-1, -1, -1, -1};
+  Eigen::Vector3d vmin = {0, 0, 0};
   static const int facets[3][3] = {{0, 1, 3}, {0, 2, 3}, {1, 2, 3}};
-  double vmin = std::numeric_limits<double>::max();
+  double qmin = std::numeric_limits<double>::max();
   for (int i = 0; i < 3; ++i)
   {
     if (FacetsTest[i + 1] == 0)
     {
-      sTmp[0].nvrtx = 3;
-      sTmp[0].vrtx.row(0) = s.vrtx.row(facets[i][0]);
-      sTmp[0].vrtx.row(1) = s.vrtx.row(facets[i][1]);
-      sTmp[0].vrtx.row(2) = s.vrtx.row(facets[i][2]);
-      std::array<int, 3> arr;
-      Eigen::Vector3d vTmp;
-      std::tie(arr, vTmp) = S2D(sTmp[0], 0, 1, 2);
-      int n = 0;
-      for (n = 0; n < 3; ++n)
-      {
-        if (arr[n] == -1)
-          break;
-        sTmp[0].vrtx.row(n) = sTmp[0].vrtx.row(arr[n]);
-      }
-      sTmp[0].nvrtx = n;
+      auto [arr, v] = S2D(s, facets[i][0], facets[i][1], facets[i][2]);
 
-      const double vnorm = vTmp.squaredNorm();
-      if (vnorm < vmin)
+      const double q = v.squaredNorm();
+      if (q < qmin)
       {
-        vmin = vnorm;
-        vBest = vTmp;
-        sTmp[1] = sTmp[0];
+        qmin = q;
+        vmin = v;
+        arrmin = arr;
       }
     }
   }
-  s = sTmp[1];
-  return vBest;
+  return {arrmin, vmin};
 }
-
+//-------------------------------------------------------------------------------
 Eigen::Vector3d
 support(const Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor>& bd,
         const Eigen::Vector3d& v)
@@ -281,10 +249,11 @@ gjk(const Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor>& bd1,
   double eps_rel = 1e-12;
 
   // Initialise
-  Simplex s;
-  s.nvrtx = 1;
+  Eigen::Matrix<double, 4, 3, Eigen::RowMajor> s;
+  int nvrtx = 1;
+  std::array<int, 4> arr = {0, -1, -1, -1};
   Eigen::Vector3d v = bd1.row(0) - bd2.row(0);
-  s.vrtx.row(0) = v;
+  s.row(0) = v;
 
   // Begin GJK iteration
   int k;
@@ -295,11 +264,10 @@ gjk(const Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor>& bd1,
 
     // Break if any existing points are the same as w
     int m;
-    for (m = 0; m < s.nvrtx; ++m)
-      if (s.vrtx(m, 0) == w[0] and s.vrtx(m, 1) == w[1]
-          and s.vrtx(m, 2) == w[2])
+    for (m = 0; m < nvrtx; ++m)
+      if (s(m, 0) == w[0] and s(m, 1) == w[1] and s(m, 2) == w[2])
         break;
-    if (m != s.nvrtx)
+    if (m != nvrtx)
       break;
 
     // 1st exit condition (v-w).v = 0
@@ -310,38 +278,29 @@ gjk(const Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor>& bd1,
       break;
 
     // Add new vertex to simplex
-    assert(s.nvrtx != 4);
-    s.vrtx.row(s.nvrtx) = w;
-    ++s.nvrtx;
+    s.row(nvrtx) = w;
+    ++nvrtx;
 
-    std::array<int, 3> arr;
-    int n = 0;
     // Invoke distance sub-algorithm
-    switch (s.nvrtx)
+    switch (nvrtx)
     {
     case 4:
-      v = S3D(s);
+      std::tie(arr, v) = S3D(s);
       break;
     case 3:
       std::tie(arr, v) = S2D(s, 0, 1, 2);
-      for (n = 0; n < 3; ++n)
-      {
-        if (arr[n] == -1)
-          break;
-        s.vrtx.row(n) = s.vrtx.row(arr[n]);
-      }
-      s.nvrtx = n;
       break;
     case 2:
       std::tie(arr, v) = S1D(s, 0, 1);
-      for (n = 0; n < 3; ++n)
-      {
-        if (arr[n] == -1)
-          break;
-        s.vrtx.row(n) = s.vrtx.row(arr[n]);
-      }
-      s.nvrtx = n;
       break;
+    }
+
+    // Rebuild s
+    for (nvrtx = 0; nvrtx < 3; ++nvrtx)
+    {
+      if (arr[nvrtx] == -1)
+        break;
+      s.row(nvrtx) = s.row(arr[nvrtx]);
     }
 
     // 2nd exit condition - intersecting or touching
--- lib/src/openGJK.hpp
+++ lib/src/openGJK.hpp
@@ -3,18 +3,6 @@
 
 #include <Eigen/Dense>
 
-///
-/// @brief Structure for a simplex.
-///
-using Simplex = struct Simplex
-{
-  /// Vertex coordinates
-  Eigen::Matrix<double, 4, 3, Eigen::RowMajor> vrtx;
-
-  /// Number of active vertices (1=point, 2=line, 3=triangle, 4=tetrahedron)
-  int nvrtx;
-};
-
 /// Calculate the distance between two convex bodies bd1 and bd2
 /// @param[in] bd1 Body 1 list of vertices
 /// @param[in] bd2 Body 2 list of vertices
